1c2f8003c4cda9e6613f8bba3b7e6d3a6f77c2bd
KiwiKernJP <165952958+KiwiKernJP@users.noreply.github.com>
2024-04-05 16:19:44 +0200

    implemented solution.py

--- sheets/01_cpsat/exercises/01_multi_knapsack/solution.py
+++ sheets/01_cpsat/exercises/01_multi_knapsack/solution.py
@@ -32,8 +32,21 @@ class MultiKnapsackSolver:
         self.solver = CpSolver()
         self.solver.parameters.log_search_progress = True
         # TODO: Implement me!
+        #Variable
+        self.x = {}
+        for i in range(len(self.capacities)):
+            for j in range(len(self.items)):
+        # self.x[i, j] = [self.model.NewBoolVar(f"x_{i}_{j}") for j in range(len(self.capacities))]
+                self.x[i, j] = self.model.NewBoolVar(f"x_{i}_{j}")
+        #Constraints
+        for i in range(len(self.capacities)):
+            self.model.Add(sum(self.x[i, j] * self.items[j].weight for j in range(len(self.items))) <= self.capacities[i])
+            #self.model.Add(sum(x[i][j] * i.weight for x, i in zip(self.x, self.items)) <= self.capacities[j])
+        for j in range(len(self.items)):
+            self.model.Add(sum(self.x[i, j] for i in range(len(self.capacities))) <= 1)
 
-
+        for j in range(len(self.items)):
+            self.model.Maximize(sum(self.items[j].value * self.x[i, j] for j in range(len(self.items)) for i in range(len(self.capacities))))
 
     def solve(self, timelimit: float = math.inf) -> Solution:
         """
@@ -51,4 +64,20 @@ class MultiKnapsackSolver:
         elif timelimit < math.inf:
             self.solver.parameters.max_time_in_seconds = timelimit
         # TODO: Implement me!
-        return Solution(knapsacks=[])  # empty solution
+        status = self.solver.Solve(self.model)
+
+        tmp3 = range(len(self.capacities))
+        knapsack = list()
+        for i in range(len(self.capacities)):
+            knapsack.append(list())
+        k = 0
+       # selected_values1 = [self.solver.Value(var) for var in x]
+        for i in range(len(self.capacities)):
+            for j in range(len(self.items)):
+                tmp4 = self.solver.Value(self.x[i, j])
+                if (self.solver.Value(self.x[i, j]) == 1):
+
+                    knapsack[i].append(self.items[j])
+                    k = k+1
+
+        return Solution(knapsacks=knapsack)  # empty solution

c8ca2b58e30d721126763db07b7aa3ad979dfef1
KiwiKernJP <165952958+KiwiKernJP@users.noreply.github.com>
2024-04-05 16:21:12 +0200

    Implemented solution_python.py

--- sheets/01_cpsat/exercises/00_foobar/solution_python.py
+++ sheets/01_cpsat/exercises/00_foobar/solution_python.py
@@ -5,6 +5,12 @@ def solve(instance: Instance) -> Solution:
     """
     Implement your solver for the problem here!
     """
+    numbersToCheck = instance.numbers
+    minimum = min(numbersToCheck)
+    maximum = max(numbersToCheck)
+    instance.numbers[0] = minimum
+    instance.numbers[-1] = maximum
+
     numbers = instance.numbers
     return Solution(
         number_a=numbers[0],
